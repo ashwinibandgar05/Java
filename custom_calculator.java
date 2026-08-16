f6e9ac9fc8b7610c88c8526d4149b6d8d7eca565
Ashwini Bandgar <ashwinibandgar95@gmail.com>
2026-08-16 12:52:06 +0530

    finally Block

---
+++ custom_calculator.java
@@ -0,0 +1,17 @@
+public class custom_calculator {
+    public static void main(String[] args){
+        /*
+        Exercise : You have to creat ea custom calculator with following operations:
+        1) ->Addition
+        2)->Substraction
+        3) ->Multiplication
+        4) ->Division
+        Which throws the following Eceptions
+        1. invalid input eception ex:9,10
+        2. cannot divide by 0 exceptoion
+        3.max  input exception if any of the inputs is greater than 1000
+        4.max multiplier reached exception -don not allow any multiplicatioj input to be greater than 7000 
+         */
+        
+    }
+}
