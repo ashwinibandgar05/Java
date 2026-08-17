3ab8f62863428a4162e08ad4689b690f972c51d3
Ashwini Bandgar <ashwinibandgar95@gmail.com>
2026-08-17 18:39:38 +0530

    Date Time class

--- TimeDate.java
+++ TimeDate.java
@@ -1,5 +1,17 @@
+import java.util.Date;
+
 public class TimeDate{
     public static void main(String[] args){
         System.out.println(System.currentTimeMillis()/1000/3600/24/365);
+        Date d=new Date();
+        System.out.println(d);
+        System.out.println(d.getTime());
+        System.out.println(d.getDate());
+        System.out.println(d.getSeconds());
+        System.out.println(d.getYear());
+        
+        
+        
+
     }
 }
