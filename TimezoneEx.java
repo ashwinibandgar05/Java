d5a7ed88ceb5404e55a4ae3829167106164bce3d
Ashwini Bandgar <ashwinibandgar95@gmail.com>
2026-08-17 19:17:57 +0530

    Timezone Class

---
+++ TimezoneEx.java
@@ -0,0 +1,9 @@
+import java.util.TimeZone;
+
+public class TimezoneEx {
+    public static void main(String[] args){
+        System.out.println(TimeZone.getAvailableIDs()[0]);
+        System.out.println(TimeZone.getAvailableIDs()[1]);
+        System.out.println(TimeZone.getAvailableIDs()[2]);
+    }
+}
